bc48c2daefb42bb39abddf7f2e00a68771f20d3a
Murilo Fragoso <mu_fragoso@hotmail.com>
2020-10-23 22:10:32 -0300

    Commit inicial

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+      <span>Desenvolvido <strong>5º ADS Noturno</strong>© 2020</span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer{
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding-right: 30px;
+    }
+</style>
---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class="header">
+        <a class="toggle">
+            <i class="fas fa-arrow-down"></i>
+        </a>
+        <h1 class="title">
+            {{ title }}
+        </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        hideToggle: Boolean
+    }
+}
+</script>
+
+<style>
+    .header{
+        grid-area: header;
+        background: linear-gradient(to right, #56CCF2, #2F80ED);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
