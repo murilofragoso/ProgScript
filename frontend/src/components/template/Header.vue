5face0d7b318faf3924cbe8c27c6a81e4e21a329
Murilo Fragoso <mu_fragoso@hotmail.com>
2020-11-06 22:33:55 -0300

    Aula 06/11/2020

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -1,20 +1,44 @@
 <template>
     <header class="header">
         <a class="toggle">
-            <i class="fas fa-arrow-down"></i>
+            <i 
+                class="fa fa-lg" 
+                :class="icon" 
+                @click="toggleMenu"
+                v-if="!hideToggle"
+            ></i>
         </a>
         <h1 class="title">
             {{ title }}
         </h1>
+        <UserDropdown v-if="!hideUserDropdown"/>
     </header>
 </template>
 
 <script>
+import UserDropdown from './UserDropdown'
+
 export default {
     name: 'Header',
+    components: {
+        UserDropdown
+    },
     props: {
         title: String,
-        hideToggle: Boolean
+        hideToggle: Boolean,
+        hideUserDropdown: Boolean
+    },
+    computed: {
+        icon(){
+            return this.$store.state.isMenuVisible 
+                ? "fa-angle-left" 
+                : "fa-angle-down"
+        }
+    },
+    methods: {
+        toggleMenu () {
+            this.$store.commit('toggleMenu')
+        }
     }
 }
 </script>
@@ -26,5 +50,35 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+
+    .title{
+        font-size: 1.2rem;
+        color: white;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+    }
+
+    .title a {
+        color: white;
+        text-decoration: none;
+    }
+
+    header.header > a.toggle{
+        width: 60px;
+        height: 100%;
+        color: white;
+        justify-self: flex-start;
+        text-decoration: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header.header > a.toggle:hover {
+        color: white;
+        background-color: rgba(0, 0, 0, 0.2);
+        cursor: pointer;
     }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import 'font-awesome/css/font-awesome.css'
 
 import App from './App'
+import store from './config/store'
+import './config/bootstrap'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
